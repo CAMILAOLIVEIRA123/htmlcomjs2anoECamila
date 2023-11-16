d3cfe36e9576fc3c95255a1ba785639fa64c7753
CAMILAOLIVEIRA123 <128100831+CAMILAOLIVEIRA123@users.noreply.github.com>
2023-11-16 20:26:23 -0300

    Update main.js
    1611

--- main.js
+++ main.js
@@ -1,5 +1,26 @@
-function tocaSomPom () {
-    document.querySelector('#som_tecla_pom').play();
+function tocaSompom (idElementoAudio)  {
+    Documento.querySelector (idElementoAudio).play();
 }
 
-document.querySelector('.tecla_pom').onclick = tocaSomPom;
+const listaDeteclas = document. querySelectorAll('.tecla ') ;
+
+//para
+for (let contador = 0; contador <listaDeteclas. length;contador++)
+
+const tecla= listaDeteclas[contador];
+const instrumento = tecla.classList[1];
+const idAudio = `#som_${instrumento}` ;//template string
+
+tecla.onclik = function() {
+    tocaSompom(idAudio);
+
+
+tecla.onkeydown = function (evento) {
+
+console.log()
+
+
+tecla.onkeydown =function (){
+    tecla.classList
+
+
